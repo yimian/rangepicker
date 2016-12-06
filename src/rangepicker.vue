9f013785e4529a8f52a7f490edecd10161bc101d
周宇松 <zedzhou@zz-2.local>
2016-12-06 16:54:13 +0800

    add: delay prop

--- src/rangepicker.vue
+++ src/rangepicker.vue
@@ -29,6 +29,9 @@
         width:50%;
         padding:4px;
         display:inline-block;
+        .disabled {
+          opacity: .4;
+        }
       }
       .btn {
         width:100%;
@@ -66,10 +69,14 @@
       </div>
       <div class="options">
         <div id="absolute-date" :class="{ 'hidden': !isActive }">
-          <span class="options-btn" v-for="metric in options.absolute"><button type="button" class="btn btn-default" @click="changeDateRange(metric)">{{ metric.name }}</button></span>
+          <span class="options-btn" v-for="metric in options.absolute" v-if="metric.available">
+              <button type="button" class="btn btn-default" :class="{ 'disabled': metric.disabled }" @click="changeDateRange(metric)">{{ metric.name }}</button>
+          </span>
         </div>
         <div id="relative-date" :class="{ 'hidden': isActive }">
-          <span class="options-btn" v-for="metric in options.relative"><button type="button" class="btn btn-default" @click="changeDateRange(metric)">{{ metric.name }}</button></span>
+          <span class="options-btn" v-for="metric in options.relative" v-if="metric.available">
+              <button type="button" class="btn btn-default" :class="{ 'disabled': metric.disabled }" @click="changeDateRange(metric)">{{ metric.name }}</button>
+          </span>
         </div>
       </div>
       <div class="range-picker">
@@ -82,173 +89,194 @@
 </template>
 
 <script>
-  import moment from 'moment'
-  import datepicker from './datepicker.vue'
-  export default {
-    props: {
-      language: {
-        type: String,
-        required: false,
-        default: 'ch'
-      },
-      adjustBy: {
-        type: String,
-        required: false,
-        default: 'day',
-      },
-      hasAdjuster: {
-        type: Boolean,
-        required: false,
-        default: false,
-      },
-      dateFormat: {
-        type: String,
-        required: false,
-        default: 'YYYY-MM-DD'
-      },
-      startDate: {
-        type: String,
-        required: true,
-        twoWay: true,
-        default: moment().format('YYYY-MM-DD')
-      },
-      endDate: {
-        type: String,
-        required: true,
-        twoWay: true,
-        default: moment().format('YYYY-MM-DD')
-      },
-      startMin: {
-        type: String,
-        required: false,
-        default: '1970-01-01',
-      },
-      endMin: {
-        type: String,
-        required: false,
-        default: '1970-01-01'
-      },
-      startMax: {
-        type: String,
-        required: false,
-        default: moment().format('YYYY-MM-DD')
-      },
-      endMax: {
-        type: String,
-        required: false,
-        default: moment().format('YYYY-MM-DD')
-      },
-      absoluteOption: {
-        type: Array,
-        required: false,
-        default: function() {
-          return [
-          ];
-        }
-      },
-      relativeOption: {
-        type: Array,
-        required: false,
-        default: function() {
-          return [
-          ];
-        }
-      }
-    },
-    data() {
-      return {
-        toggleRangePicker: false,
-        coordinates: {},
-        options: {
-          absolute: [
-            {'name': '今天', 'value': 0, 'unit': 'day', 'type': 'absolute', 'weekFormat': 'isoweek'},
-            {'name': '昨天', 'value': 1, 'unit': 'day', 'type': 'absolute', 'weekFormat': 'isoweek'},
-            {'name': '本周', 'value': 0, 'unit': 'week', 'type': 'absolute', 'weekFormat': 'isoweek'},
-            {'name': '上周', 'value': 1, 'unit': 'week', 'type': 'absolute', 'weekFormat': 'isoweek'},
-            {'name': '本月', 'value': 0, 'unit': 'month', 'type': 'absolute', 'weekFormat': 'isoweek'},
-            {'name': '上月', 'value': 1, 'unit': 'month', 'type': 'absolute', 'weekFormat': 'isoweek'},
-          ],
-          relative: [
-            {'name': '过去7天', 'value': 7, 'unit': 'day', 'type': 'relative', 'weekFormat': 'isoweek'},
-            {'name': '过去30天', 'value': 30, 'unit': 'day', 'type': 'relative', 'weekFormat': 'isoweek'},
-            {'name': '过去3个月', 'value': 3, 'unit': 'month', 'type': 'relative', 'weekFormat': 'isoweek'},
-            {'name': '过去半年', 'value': 0.5, 'unit': 'year', 'type': 'relative', 'weekFormat': 'isoweek'},
-          ],
-        },
-        isActive: true,
-      }
-    },
-    watch: {
-        startDate(v) {
-            this.endMin = this.startDate;
-        },
-        endDate(v) {
-            this.startMax = this.endDate;
-        },
-    },
-    ready() {
-        var self = this;
-        this.absoluteOption.map(function (option) {
-            self.options.absolute.push(option);
-        })
-        this.relativeOption.map(function (option) {
-            self.options.relative.push(option);
-        })
-
-        this.update_coords();
-        // 检测窗口是否变化，如果发生变化重新对下拉框进行定位
-        window.addEventListener('resize', this.update_coords);
-        window.addEventListener('scroll', this.update_coords);
-
-        window.addEventListener('click', this.close)
-    },
-    beforeDestroy() {
-        window.removeEventListener('click', this.close)
-    },
-    components: { datepicker },
-    methods: {
-        update_coords() {
-            var rect = this.$el.getBoundingClientRect();
-            var top = this.$els.input.offsetHeight + rect.top;
-            this.coordinates = {left: rect.left + 'px', width: rect.width + 'px', top: top + 'px'};
-        },
-        adjustRangeBy(term) {
-            term = term || term in ['day', 'isoweek', 'month', 'quarter', 'year'] ? term : 'day';
-            this.startDate = moment(this.startDate).startOf(term).format(this.dateFormat);
-            this.endDate = moment(this.endDate).endOf(term).format(this.dateFormat);
-            if (moment(this.endDate) > moment()) {
-                this.endDate = moment().format(this.dateFormat);
+    import moment from 'moment'
+    import datepicker from './datepicker.vue'
+    export default {
+        props: {
+            language: {
+                type: String,
+                required: false,
+                default: 'ch'
+            },
+            dateFormat: {
+                type: String,
+                required: false,
+                default: 'YYYY-MM-DD'
+            },
+            delay: {
+                type: Number,
+                required: false,
+                default: 0
+            },
+            startDate: {
+                type: String,
+                required: true,
+                twoWay: true,
+                default: moment().format('YYYY-MM-DD')
+            },
+            endDate: {
+                type: String,
+                required: true,
+                twoWay: true,
+                default: moment().format('YYYY-MM-DD')
+            },
+            startMin: {
+                type: String,
+                required: false,
+                default: '1970-01-01',
+            },
+            endMin: {
+                type: String,
+                required: false,
+                default: '1970-01-01'
+            },
+            startMax: {
+                type: String,
+                required: false,
+                default: moment().format('YYYY-MM-DD')
+            },
+            endMax: {
+                type: String,
+                required: false,
+                default: moment().format('YYYY-MM-DD')
+            },
+            absoluteOption: {
+                type: Array,
+                required: false,
+                default: function() {
+                    return [];
+                }
+            },
+            relativeOption: {
+                type: Array,
+                required: false,
+                default: function() {
+                    return [];
+                }
             }
         },
-        toggle_range_picker() {
-            this.toggleRangePicker = !this.toggleRangePicker;
-        },
-
-        switchTab() {
-            this.isActive = !this.isActive;
-        },
-
-        close(e) {
-            // let rangepickerEl = this.$el.getElementsByClassName('rangepicker')[0];
-            if(!this.$el.contains(e.target)){
-                this.toggleRangePicker = false;
+        data() {
+            return {
+                toggleRangePicker: false,
+                coordinates: {},
+                options: {
+                    absolute: [
+                        {'name': '今天', 'value': 0, 'unit': 'day', 'type': 'absolute', 'weekFormat': 'isoweek', 'available': false, 'disabled': false},
+                        {'name': '昨天', 'value': 1, 'unit': 'day', 'type': 'absolute', 'weekFormat': 'isoweek', 'available': false, 'disabled': false},
+                        {'name': '本周', 'value': 0, 'unit': 'week', 'type': 'absolute', 'weekFormat': 'isoweek', 'available': true, 'disabled': false},
+                        {'name': '上周', 'value': 1, 'unit': 'week', 'type': 'absolute', 'weekFormat': 'isoweek', 'available': true, 'disabled': false},
+                        {'name': '本月', 'value': 0, 'unit': 'month', 'type': 'absolute', 'weekFormat': 'isoweek', 'available': true, 'disabled': false},
+                        {'name': '上月', 'value': 1, 'unit': 'month', 'type': 'absolute', 'weekFormat': 'isoweek', 'available': true, 'disabled': false},
+                    ],
+                    relative: [
+                        {'name': '过去7天', 'value': 7, 'unit': 'day', 'type': 'relative', 'weekFormat': 'isoweek', 'available': true, 'disabled': false},
+                        {'name': '过去30天', 'value': 30, 'unit': 'day', 'type': 'relative', 'weekFormat': 'isoweek', 'available': true, 'disabled': false},
+                        {'name': '过去3个月', 'value': 3, 'unit': 'month', 'type': 'relative', 'weekFormat': 'isoweek', 'available': true, 'disabled': false},
+                        {'name': '过去半年', 'value': 0.5, 'unit': 'year', 'type': 'relative', 'weekFormat': 'isoweek', 'available': true, 'disabled': false},
+                    ],
+                },
+                isActive: true,
             }
         },
+        watch: {
+            startDate(v) {
+                this.endMin = this.startDate;
+            },
+            endDate(v) {
+                this.startMax = this.endDate;
+            },
+        },
+        ready() {
+            this.endMax = moment(this.endMax).subtract(this.delay, 'day').format(this.dateFormat);
+            this.startDate = moment(this.startDate).subtract(this.delay, 'day').format(this.dateFormat);
+            this.endDate = moment(this.endDate).subtract(this.delay, 'day').format(this.dateFormat);
+            var self = this;
+            this.absoluteOption.map(function (option) {
+                self.options.absolute.push(option);
+            })
+            this.relativeOption.map(function (option) {
+                self.options.relative.push(option);
+            })
 
-        changeDateRange(metric) {
-            if (metric.type === 'absolute') {
-                this.startDate = moment().startOf(metric.unit === 'week' ? metric.weekFormat : metric.unit).subtract(metric.value, metric.unit).format(this.dateFormat);
-                if (metric.value) {
-                    this.endDate = moment().subtract(metric.value, metric.unit).endOf(metric.unit === 'week' ? metric.weekFormat : metric.unit).format(this.dateFormat);
-                } else {
+            // init options, display if disabled
+            this.options.absolute.map(function (metric) {
+                var validMaxDate = moment().subtract(self.delay, 'day');
+                var metricStartDate = moment().startOf(metric.unit === 'week' ? metric.weekFormat : metric.unit).subtract(metric.value, metric.unit);
+                if (metricStartDate > validMaxDate) {
+                    metric.disabled = true;
+                }
+            })
+
+            this.options.relative.map(function (metric) {
+                var validMaxDate = moment().subtract(self.delay, 'day');
+                var metricStartDate = moment().subtract(metric.value, metric.unit);
+                if (metricStartDate > validMaxDate) {
+                    metric.disabled = true;
+                }
+            })
+
+            this.update_coords();
+            // 检测窗口是否变化，如果发生变化重新对下拉框进行定位
+            window.addEventListener('resize', this.update_coords);
+            window.addEventListener('scroll', this.update_coords);
+
+            window.addEventListener('click', this.close)
+        },
+        beforeDestroy() {
+            window.removeEventListener('click', this.close)
+        },
+        components: { datepicker },
+        methods: {
+            update_coords() {
+                var rect = this.$el.getBoundingClientRect();
+                var top = this.$els.input.offsetHeight + rect.top;
+                this.coordinates = {left: rect.left + 'px', width: rect.width + 'px', top: top + 'px'};
+            },
+            adjustRangeBy(term) {
+                term = term || term in ['day', 'isoweek', 'month', 'quarter', 'year'] ? term : 'day';
+                this.startDate = moment(this.startDate).startOf(term).format(this.dateFormat);
+                this.endDate = moment(this.endDate).endOf(term).format(this.dateFormat);
+                if (moment(this.endDate) > moment()) {
                     this.endDate = moment().format(this.dateFormat);
                 }
-            } else if (metric.type === 'relative') {
-                this.startDate = moment().subtract(metric.value, metric.unit).format(this.dateFormat);
-                this.endDate = moment().format(this.dateFormat);
+            },
+            toggle_range_picker() {
+                this.toggleRangePicker = !this.toggleRangePicker;
+            },
+
+            switchTab() {
+                this.isActive = !this.isActive;
+            },
+
+            close(e) {
+                // let rangepickerEl = this.$el.getElementsByClassName('rangepicker')[0];
+                if(!this.$el.contains(e.target)){
+                    this.toggleRangePicker = false;
+                }
+            },
+
+            changeDateRange(metric) {
+                if (!metric.disabled) {
+                    var validMaxDate = moment().subtract(this.delay, 'day');
+                    if (metric.type === 'absolute') {
+                        this.startDate = moment().startOf(metric.unit === 'week' ? metric.weekFormat : metric.unit).subtract(metric.value, metric.unit).format(this.dateFormat);
+                        if (metric.value) {
+                            var metricEndDate = moment().subtract(metric.value, metric.unit).endOf(metric.unit === 'week' ? metric.weekFormat : metric.unit);
+                            if (validMaxDate < metricEndDate) {
+                                this.endDate = validMaxDate.format(this.dateFormat);
+                            } else {
+                                this.endDate = metricEndDate.format(this.dateFormat);
+                            }
+                        } else {
+                            this.endDate = validMaxDate.format(this.dateFormat);
+                        }
+                    } else if (metric.type === 'relative') {
+                        this.startDate = moment().subtract(metric.value, metric.unit).format(this.dateFormat);
+                        this.endDate = validMaxDate.format(this.dateFormat);
+                    }
+                    this.toggle_range_picker();
+                }
             }
-            this.toggle_range_picker();
-        }
-    },
-  }
+        },
+    }
 </script>
